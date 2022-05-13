14430c8cb2a693e1d8b608aa264a28731ab7c5e0
samuelnfavero <faveronsamuel@gmail.com>
2022-05-13 15:34:50 -0300

    refactor: Refactorando o código para melhorar a legibilidade

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Solution {
 
@@ -15,75 +14,89 @@ public class Solution {
 
         for (int i = 0; i < requirementList.size(); i++) {
 
-            int melhorQuarto = -1;
+            int bestFlat = -1;
 
-            List posicoes = new ArrayList<Integer>();
-            for (int j = 0; j < roomsList.size(); j++) {
+            List<Integer> roomsPosition = getFlatsWithEnoughtRooms(roomsList, requirementList, i);
+            removeRoomsWithHigherPrice(roomsPosition,rentList, budgetList, i);
 
-                if (roomsList.get(j) >= requirementList.get(i)) {
+            if (roomsPosition.size() != 0) {
 
-                    posicoes.add(j);
+                int lowerPrice = getTheLowerPrice(roomsPosition, rentList);
+                chooseTheLowestPriceRooms(roomsPosition, rentList,lowerPrice);
+
+                int flatWithTheHighestNumberOfRooms = roomsList.get(roomsPosition.get(0));
+                bestFlat = chooseTheFlatWithTheHighestNumberOfRoomsWithTheBestPrice(roomsPosition, roomsList, flatWithTheHighestNumberOfRooms, bestFlat);
+
+                if (bestFlat != -1) {
+                    System.out.printf("O cliente %s ficou com o quarto %s\n", i, bestFlat);
+                    valor += rentList.get(bestFlat);
+                    roomsList.remove(bestFlat);
+                    rentList.remove(bestFlat);
                 }
             }
 
-            for (int k = 0; k < posicoes.size() ; k++) {
-
-                int rente =rentList.get((int) posicoes.get(k));
-                int budgete = budgetList.get(i);
-                if (rentList.get((int) posicoes.get(k)) > budgetList.get(i)) {
-
-
-
-                    posicoes.remove(Integer.valueOf((int) posicoes.get(k)));
-                    k--;
-                }
-            }
-
-            if (posicoes.size() != 0) {
-
-                int valorMaisBaixo = rentList.get((int) posicoes.get(0));
-
-                for (int l = 0; l < posicoes.size(); l++) {
-                    if (rentList.get((int) posicoes.get(l)) < valorMaisBaixo) {
-                        valorMaisBaixo = rentList.get((int) posicoes.get(l));
-                    }
-                }
-
-                for (int p = 0; p < posicoes.size(); p++) {
-                    if (rentList.get((int) posicoes.get(p)) != valorMaisBaixo) {
-                        posicoes.remove(Integer.valueOf((int) posicoes.get(p)));
-                        p--;
-                    }
-                }
-
-                int maiorNumDeQuartos = roomsList.get((int) posicoes.get(0));
-
-
-                for (int t = 0; t < posicoes.size(); t++) {
-                    if (roomsList.get((int) posicoes.get(t)) >= maiorNumDeQuartos) {
-                        maiorNumDeQuartos = roomsList.get((int) posicoes.get(t));
-                        melhorQuarto = (int) posicoes.get(t);
-                    }
-                }
-
-                if (melhorQuarto != -1) {
-                    System.out.printf("O cliente %s ficou com o quarto %s\n", i, melhorQuarto);
-                    valor += rentList.get(melhorQuarto);
-                    roomsList.remove(melhorQuarto);
-                    rentList.remove(melhorQuarto);
-                }
-            }
-
-            if(melhorQuarto == -1) System.out.printf("Nenhum quarto satisfez o cliente %s\n", i);
+            if(bestFlat == -1) System.out.printf("Nenhum quarto satisfez o cliente %s\n", i);
         }
         return valor;
     }
 
-    public ArrayList<Integer> convertToList(int[] array){
-        ArrayList list = new ArrayList<Integer>();
-        for(int i = 0; i < array.length; i++){
-            list.add(array[i]);
+    private List<Integer> convertToList(int[] array){
+        List<Integer> list = new ArrayList<Integer>();
+        for (int j : array) {
+            list.add(j);
         }
         return list;
     }
+
+    private List<Integer> getFlatsWithEnoughtRooms(List<Integer> roomsList, List<Integer> requirementList, Integer clientNumber){
+        List<Integer> roomsPosition = new ArrayList<Integer>();
+        for (int i = 0; i < roomsList.size(); i++) {
+
+            if (roomsList.get(i) >= requirementList.get(clientNumber)) {
+
+                roomsPosition.add(i);
+            }
+        }
+        return  roomsPosition;
+    }
+
+    private void removeRoomsWithHigherPrice(List<Integer> roomPosition, List<Integer> rentList, List<Integer> budgetList, Integer clientNumber){
+        for (int k = 0; k < roomPosition.size() ; k++)
+            if (rentList.get((int) roomPosition.get(k)) > budgetList.get(clientNumber)) {
+                roomPosition.remove(Integer.valueOf((int) roomPosition.get(k)));
+                k--;
+            }
+    }
+
+    private int getTheLowerPrice(List<Integer> positions, List<Integer> rentList){
+        int menorValor = rentList.get((int) positions.get(0));
+
+        for (Integer p : positions) {
+            if (rentList.get((int) p) < menorValor) {
+                menorValor = rentList.get((int) p);
+            }
+        }
+        return menorValor;
+    }
+
+    private void chooseTheLowestPriceRooms(List<Integer> roomPosition, List<Integer> rentList, int lowerPrice){
+        for (int p = 0; p < roomPosition.size(); p++) {
+            if (rentList.get((int) roomPosition.get(p)) != lowerPrice) {
+                roomPosition.remove(Integer.valueOf((int) roomPosition.get(p)));
+                p--;
+            }
+        }
+    }
+
+    private int chooseTheFlatWithTheHighestNumberOfRoomsWithTheBestPrice(List<Integer> roomsPosition, List<Integer> roomsList,
+                                                                         int flatWithTheHighestNumberOfRooms, int melhorQuarto){
+        for (Integer room : roomsPosition) {
+            if (roomsList.get(room) >= flatWithTheHighestNumberOfRooms) {
+                flatWithTheHighestNumberOfRooms = roomsList.get(room);
+                melhorQuarto = room;
+            }
+        }
+        return melhorQuarto;
+    }
+    private void removeNotEnoughtRooms(){}
 }
